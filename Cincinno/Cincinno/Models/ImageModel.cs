79928cb1c06e3def9bf010c92db630228c5a8088
gregofoegbu <86071125+gregofoegbu@users.noreply.github.com>
2024-03-13 17:30:30 -0230

    Fixed Naming Issue

---
+++ Cincinno/Cincinno/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Cincinno.Models
+{
+	public class ImageModel
+	{
+		public int Id { get; set; }
+		public Guid UserId { get; set; }
+		public string Filename { get; set; }
+		public byte[] Data { get; set; }
+		public string Name { get; set; }
+
+		public ImageModel()
+		{
+		}
+	}
+}
+
